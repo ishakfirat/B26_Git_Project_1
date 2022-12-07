766d5e3e297d6744232d4cd1f655b87ac57ddd74
Ishak Firat <iishakfirat@gmail.com>
2022-12-07 22:28:10 +0000

    Wrote the code for First class

--- src/day0/First.java
+++ src/day0/First.java
@@ -6,6 +6,12 @@ public class First {
 
         System.out.println("First day in Java");
 
+
+
+    }
+
+    public static void sum(int num1,int num2){
+        System.out.println(num1+num2);
     }
 
 }
